a95af4e40ef5b1995b332cdd2cc6d99a290fe2e0
Michael Wells <mwells@sygnal.com>
2021-08-26 11:40:46 +1200

    Initial cookie-based auth implementation

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// For Blazor Server Cookie Auth
+// https://blazorhelpwebsite.com/ViewBlogPost/36
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+
 namespace BlazorServerAuth
 {
     public class Startup
@@ -26,9 +32,31 @@ namespace BlazorServerAuth
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // For Blazor Server Cookie Auth
+            // https://blazorhelpwebsite.com/ViewBlogPost/36
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddAuthentication(
+                CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie();
+
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            // For Blazor Server Cookie Auth
+            // https://blazorhelpwebsite.com/ViewBlogPost/36
+            // From: https://github.com/aspnet/Blazor/issues/1554
+            // HttpContextAccessor
+            services.AddHttpContextAccessor();
+            services.AddScoped<HttpContextAccessor>();
+            services.AddHttpClient();
+            services.AddScoped<HttpClient>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +77,13 @@ namespace BlazorServerAuth
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            // For Blazor Server Cookie Auth
+            // https://blazorhelpwebsite.com/ViewBlogPost/36
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
